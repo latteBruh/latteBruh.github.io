27d5c3f0152b738d417c06de3e90ed5412636d54
Camden Conrad <90994136+latteBruh@users.noreply.github.com>
2022-05-17 15:18:41 -0400

    stripped, ignore case

--- isScripts.js
+++ isScripts.js
@@ -3,11 +3,11 @@ function sayHey() {
 }
 
 function rateName() {
-    let name = document.getElementById("textarea").value;
+    let name = document.getElementById("textarea").value.replace(/\s+/g, '').toLowerCase();
 
-    if (name === 'Camden') {
+    if (name === 'camden' || name === 'cam') {
         document.getElementById("rating").innerHTML = "Rating: 5/5";
-    } else if (name === 'Cara') {
+    } else if (name === 'cara') {
         document.getElementById("rating").innerHTML = "Rating: 0/5";
     } else
         document.getElementById("rating").innerHTML = "Rating: 3/5";
